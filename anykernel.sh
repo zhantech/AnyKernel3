59c8f52f053e47934cdf063f00210fabb386e095
zhantech <hifzhan41@gmail.com>
2021-03-20 11:52:53 +0000

    BringUp GatotKaca Kernel

--- anykernel.sh
+++ anykernel.sh
@@ -5,7 +5,7 @@
 # begin properties
 script="$0"
 properties() { '
-kernel.string=Luuvy kernel from Zamrud Khatulistiwa
+kernel.string=GatotKaca kernel by ZHANtech™
 do.devicecheck=1
 do.modules=0
 do.systemless=1
