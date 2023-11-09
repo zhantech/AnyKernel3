a73b7e18bf8ba6fad2a1a165e46d54728414e799
zhantech <hifzhan41@gmail.com>
2023-11-09 15:23:59 +0000

    Anykernel3 for Topaz / Tapas

--- anykernel.sh
+++ anykernel.sh
@@ -4,42 +4,52 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=GatotKaca Kernel by ZHANtech™
+kernel.string=Pringgodani Kernel
 do.devicecheck=1
-do.modules=1
-do.systemless=0
+do.modules=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=ginkgo
-device.name2=willow
+device.name1=topaz
+device.name2=tapas
 device.name3=
 device.name4=
 device.name5=
-supported.versions=10 - 11
-supported.patchlevels=
+supported.versions=
 '; } # end properties
 
 # shell variables
-block=/dev/block/bootdevice/by-name/boot;
-is_slot_device=0;
+slot=$(find_slot);
+block=/dev/block/bootdevice/by-name/boot$slot;
+is_slot_device=1;
 ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
 
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 set_perm_recursive 0 0 755 644 $ramdisk/*;
 set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
-
 ## AnyKernel install
-dump_boot;
+split_boot;
 
+## Selinux Permissive
+#patch_cmdline androidboot.selinux androidboot.selinux=permissive
 
-write_boot;
+# migrate from /overlay to /overlay.d to enable SAR Magisk
+if [ -d $ramdisk/overlay ]; then
+  rm -rf $ramdisk/overlay;
+fi;
+
+# end ramdisk changes
+
+flash_boot;
 ## end install
 
+sleep 3
+ui_print "kernel installed successfully"
